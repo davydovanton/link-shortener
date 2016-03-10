363a26660904bf246abf3f7a438c196a80b76007
Anton Davydov <antondavydov.o@gmail.com>
2016-03-10 23:10:26 +0300

    Generate admin application

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,10 @@ require 'rubygems'
 require 'bundler/setup'
 require 'hanami/setup'
 require_relative '../lib/link_shortener'
+require_relative '../apps/admin/application'
 require_relative '../apps/web/application'
 
 Hanami::Container.configure do
+  mount Admin::Application, at: '/admin'
   mount Web::Application, at: '/'
 end
